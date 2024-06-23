62ea28a777d166ab3a5984a37c8c1ead4a48d297
mansachinda <devchiamadi@gmail.com>
2024-06-23 04:53:55 -0700

    Simple Interest Calculator

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,9 @@
+# Variables containing principal, rate, and time values
+principal = 1000 # representing $1000
+rate = 0.05 # representing 5% annual interest rate
+time = 3 # representing 3 years
+
+# Calculates the Simple Interest of PRT
+Interest = principal * rate * time
+# Prints the result of Simple Interest
+print("The simple interest is:", Interest)
