3f2afcc0e84f59d256fab538bd9cc90e0b470446
mansachinda <devchiamadi@gmail.com>
2024-07-01 07:01:49 -0700

    0. Weather Recommendation Program

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,16 +1,17 @@
-# Prompt User Input for Weather
-weather = input("What's the weather like today? (sunny/rainy/cold): ")
+# Prompt User Input for Weather with 'current_weather' variable
+current_weather = input("What's the weather like today? (sunny/rainy/cold): ")
 # Variables containing 'sunny', 'rainy', 'cold'
 sunny = "sunny"
 rainy = "rainy"
 cold = "cold"
 
 # Control-Flow using 'if', 'elif', 'else' conditions
-if weather == sunny:
+if current_weather == sunny:
     print("Wear a t-shirt and sunglasses.")
-elif weather == rainy:
+elif current_weather == rainy:
     print("Don't forget your umbrella and a raincoat.")
-elif weather == cold:
+elif current_weather == cold:
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
+
