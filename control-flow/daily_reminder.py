3e1ed87cbd6827fb99bae1208c0f5dc04f811212
mansachinda <devchiamadi@gmail.com>
2024-07-01 11:39:47 -0700

    Personal Daily Reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,16 +1,30 @@
 # Prompt User Input with 'task' variable
-task = input("Enter your task: ")
-# Prompt User Input with 'priority' variable
-priority = input("Priority (high/medium/low): ")
-# Prompt User Input with 'time_bound' variable
-time_bound = str(input("Is it time-bound? (yes/no): "))
+task_reminder = input("Enter your task: ")
 
-# Use 'match case' condition to evaluate 'priority' variable 
-match task:
-    case priority:
-        # Use 'if', 'elif' condition to evaluate 'time_bound' variable
-        if time_bound == 'yes':
-            print("\nReminder:", task, "is a high priority task that requires immediate attention today!")
-        elif time_bound == 'no':
-            print("\nNote:", task, "is a low priority task. Consider completing it when you have free time.")
+# Prompt User Input with 'priority' variable
+priority = input("Priority (high/medium/low): ").lower()
+
+# Prompt User Input with 'urgency' variable
+urgency = input("Is it time-bound? (yes/no): ").lower()
+
+# Customised Reminder with 'f' format
+reminder = f"'{task_reminder}' is a {priority} priority task"
+
+# Use 'match case' condition to evaluate 'priority' variable
+match priority:
+    case "high":
+        if urgency == "yes":
+            time_bound = ""
+            reminder += time_bound if time_bound else " that requires immediate attention today!."
+            print(f"\nReminder: {reminder}")
+    case "medium":
+        if urgency == "no":
+            time_bound = ""
+            reminder += time_bound if time_bound else ". Consider completing it when you have free time."
+            print(f"\nReminder: {reminder}")
+    case "low":
+        if urgency == "no":
+            time_bound = ""
+            reminder += time_bound if time_bound else ". Consider completing it when you have free time."
+            print(f"\nNote: {reminder}")
 
