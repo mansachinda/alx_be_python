57a7363112bdc2c264c00baf27c30982599df106
mansachinda <devchiamadi@gmail.com>
2024-07-02 03:39:13 -0700

    4. Personal Daily Reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -2,29 +2,26 @@
 task_reminder = input("Enter your task: ")
 
 # Prompt User Input with 'priority' variable
-priority = input("Priority (high/medium/low): ").lower()
+task_priority = input("Priority (high/medium/low): ").lower()
 
 # Prompt User Input with 'urgency' variable
-urgency = input("Is it time-bound? (yes/no): ").lower()
+time_bound = input("Is it time-bound? (yes/no): ").lower()
 
 # Customised Reminder with 'f' format
-reminder = f"'{task_reminder}' is a {priority} priority task"
+reminder = f"'{task_reminder}' is a {task_priority} priority task"
 
-# Use 'match case' condition to evaluate 'priority' variable
-match priority:
+# 'Match Case' condition to evaluate 'task_priority' variable
+match task_priority:
     case "high":
-        if urgency == "yes":
-            time_bound = ""
-            reminder += time_bound if time_bound else " that requires immediate attention today!."
-            print(f"\nReminder: {reminder}")
+        print(f"\nReminder: {reminder}", end="")
     case "medium":
-        if urgency == "no":
-            time_bound = ""
-            reminder += time_bound if time_bound else ". Consider completing it when you have free time."
-            print(f"\nReminder: {reminder}")
+        print(f"\nReminder: {reminder}", end="")
     case "low":
-        if urgency == "no":
-            time_bound = ""
-            reminder += time_bound if time_bound else ". Consider completing it when you have free time."
-            print(f"\nNote: {reminder}")
+        print(f"\nNote: {reminder}", end="")
+
+# 'If', "Else' condition to evaluate 'time_bound' variable
+if time_bound == "yes":
+    print(" that requires immediate attention today!")
+else:
+    print(". Consider completing it when you have free time.")
 
