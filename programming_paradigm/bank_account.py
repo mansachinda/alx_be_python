71c0a48080c5dc441ee8c8ccb11db33d7a59414f
mansachinda <devchiamadi@gmail.com>
2024-07-14 06:49:34 -0700

    0. Create a Simple Bank Account Class

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,26 @@
+# 0. Create a Simple Bank Account Class
+# Program with BankAccount class with deposit() and withdraw() function
+
+# Create BankAccount class
+class BankAccount:
+    def __init__(self, initial_balance = 0):
+        self.account_balance = initial_balance
+
+# Create Deposit() function
+    def deposit(self, amount):
+        amount = input(float(f"Deposit amount: "))
+        self.account_balance += amount
+        return self.account_balance
+
+# Create Withdraw() function
+    def withdraw(self, amount):
+        amount = input(float(f"Withdraw amount: "))
+        if self.account_balance > amount:
+            self.account_balance -= amount
+            return True
+        else:
+            return False
+
+    def display_balance(self):
+        print(f"Current Balance: ${self.account_balance :.2f}")
+
